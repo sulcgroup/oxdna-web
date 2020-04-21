99b4e528c3c31cef5bed3fe1f940431250bb02f1
Roger Romero <roger.romero@neudesic.com>
2020-04-20 19:16:46 -0700

    this will be the final state of the branch

--- Account.py
+++ Account.py
@@ -7,24 +7,86 @@ import mysql.connector
 cnx = mysql.connector.connect(user='root', password='', database='azdna')
 cursor = cnx.cursor()
 
-find_email_by_user_id_query = ("SELECT email FROM Users WHERE id = %s
-set_email = ("UPDATE Users SET email = %s WHERE id = %s")
-find_date_by_user_id_query = ("SELECT creationDate FROM Users WHERE id = %s")
-find_status_by_user_id_query = ("SELECT status FROM Users WHERE id = %s")
+find_email_by_user_id_query = ("SELECT email FROM UsersDev WHERE id = %s")
+set_email = ("UPDATE UsersDev SET email = %s WHERE id = %s")
+find_date_by_user_id_query = ("SELECT creationDate FROM UsersDev WHERE id = %s")
+find_status_by_user_id_query = ("SELECT status FROM UsersDev WHERE id = %s")
+get_verify_code_query = ("SELECT verifycode FROM UsersDev WHERE id = %s")
+verify_user = ("UPDATE UsersDev SET verified = %s WHERE id = %s")
+get_username_query = ("SELECT username FROM UsersDev WHERE id = %s")
+get_userid_query = ("SELECT id FROM UsersDev WHERE username = %s")
 
+
+##DEPRECATED
 def getEmail(userId):
-	cursor.execute(find_email_by_user_id_query(userId))
-	return cursor.email
+    cursor.execute(find_email_by_user_id_query(userId))
+    return cursor.email
 
 def setEmail(email, userId):
-	cursor.execute(set_email(email, userId)
-	return "Email successfully updated!"
+    cursor.execute(set_email(email, userId))
+    return "Email successfully updated!"
+##DEPRECATED
 
 def getCreationDate(userId):
-	cursor.execute(find_date_by_user_id_query(userId))
-	return cursor.creationDate
+    cursor.execute(find_date_by_user_id_query, (userId,))
+    #fetchall returns all results
+    results = cursor.fetchall()
+    if(results):
+        return results[0][0]
+    else:
+        return None
 
 def getStatus(userId):
-	cursor.execute(find_status_by_user_id_query(userId))
-	return cursor.status
+    cursor.execute(find_status_by_user_id_query, (userId,))
+    #fetchall returns all results
+    results = cursor.fetchall()
+    if(results):
+        return results[0][0]
+    else:
+        return None
 
+def getVerificationCode(userId):
+    cursor.execute(get_verify_code_query, (userId,))
+    #fetchall returns all results
+    results = cursor.fetchall()
+    if(results):
+        return results[0][0]
+    else:
+        return None
+
+def getUsername(userId):
+    cursor.execute(get_username_query, (userId,))
+    #fetchall returns all results
+    results = cursor.fetchall()
+    if(results):
+        return results[0][0]
+    else:
+        return None
+
+def getUserId(username):
+    cursor.execute(get_userid_query, (username,))
+    #fetchall returns all results
+    results = cursor.fetchall()
+    if(results):
+        return results[0][0]
+    else:
+        return None
+
+
+#checks verification code for user
+def verifyUser(userId, VerifyCode):
+    #not sure how mysql.connector works, so wrapping in try catch just in case
+    try:
+        #query the database for the user's verification code
+        cursor.execute(get_verify_code_query, (userId,))
+        code = cursor.fetchall()
+        #check that they match
+        if(code[0][0] == VerifyCode):
+            #verify the user
+            cursor.execute(verify_user, ("True", userId))
+            cnx.commit()
+            return True;
+        else:
+            return False;
+    except:
+        return False;
--- Register.py
+++ Register.py
@@ -3,29 +3,42 @@ from datetime import date, datetime, timedelta
 import mysql.connector
 import time
 import Login
+import Account
 import bcrypt
+import os
+import binascii
+import EmailScript
 
 cnx = mysql.connector.connect(user='root', password='', database='azdna')
 cursor = cnx.cursor()
 
 
 add_user_query = (
-	"INSERT INTO Users "
-	"(`username`, `password`, `group`, `creationDate`)"
-	"VALUES (%s, %s, %s, %s)"
+"INSERT INTO UsersDev "
+"(`username`, `password`, `group`, `creationDate`, `verifycode`, `verified`)"
+"VALUES (%s, %s, %s, %s, %s, %s)"
 )
 
 #needs input cleaning/escaping/validation
-#throws no errors atm if user already exists
+
 def registerUser(name, password):
 
-	user_data = (name, bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt()), 0, int(time.time()))
-	cursor.execute(add_user_query, user_data)
-	cnx.commit()
 
-	user_id = Login.loginUser(name, password)
+    verifycode = binascii.b2a_hex(os.urandom(15)).decode("utf-8")
+    user_data = (name, bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt()), 0, int(time.time()), verifycode, "False")
+    cursor.execute(add_user_query, user_data)
+    cnx.commit()
 
-	return user_id
+    #user_id = Login.loginUser(name, password)
+    user_id = Account.getUserId(name)
+
+    #call emailing script with verification link
+    verifylink = "www.oxdna.org/verify?id={userId}&verify={verifycode}".format(userId = user_id, verifycode = verifycode)
+
+    #os.system("python3 EmailScript.py -t 0 -n {username} -u {verifylink} -d {email}".format(username = name, verifylink = verifylink, email = name))
+    EmailScript.SendEmail("-t 0 -n {username} -u {verifylink} -d {email}".format(username = name, verifylink = verifylink, email = name).split(" "))
+
+    return user_id
 
 '''
 import random
