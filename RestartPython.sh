eee891b2647f9316e96d23ffdbbe2a0e74df6fa2
Capstone Group Project <capstonegroup@oxdna.org>
2020-04-30 18:22:07 +0000

    Added restart system for the python

---
+++ RestartPython.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Running main.py"
+pkill -F /var/tmp/python.pid
+cd /var/www/azDNA/azDNA
+t=`date | cut -d ' ' -f 2,3,4 | sed 's/ /_/g'`
+mv nohup.out logs/$t
+nohup python3 /var/www/azDNA/azDNA/main.py &
+echo $! > /var/tmp/python.pid
+chmod 664 nohup.out
