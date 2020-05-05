735d463eb389d10e7f0160dfeed7e6c99c2f4a9a
Roger J. Romero <rjromero@Rogers-iMac.local>
2020-05-05 09:02:35 -0700

    add oxviewer to gitignore and fix registration for admin

--- Register.py
+++ Register.py
@@ -26,23 +26,21 @@ def registerUser(name, password, firstName, lastName, institution, requires_veri
 		cnx.close()
 		return -2 #value that is not none means user is in the database. return -2 error code.
 
-	verifycode = binascii.b2a_hex(os.urandom(15)).decode("utf-8")
-	verifylink = "http://10.126.22.10/verify?id={userId}&verify={verifycode}".format(userId = user_id, verifycode = verifycode)
-
 	user_data = (name, bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt()), 0, int(time.time()), verifycode, "False", firstName, lastName, institution)
 
-	if requires_verification:
-		EmailScript.SendEmail("-t 0 -n {username} -u {verifylink} -d {email}".format(username = name, verifylink = verifylink, email = name).split(" "))
-	else:
+	if requires_verification == False:
 		user_data = (name, bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt()), 0, int(time.time()), verifycode, "True", firstName, lastName, institution)
 		
 
 	cursor.execute(add_user_query, user_data)
 	cnx.commit()
 
-	#user_id = Login.loginUser(name, password)
 	user_id = Account.getUserId(name)
 
+	verifycode = binascii.b2a_hex(os.urandom(15)).decode("utf-8")
+	verifylink = "http://10.126.22.10/verify?id={userId}&verify={verifycode}".format(userId = user_id, verifycode = verifycode)
+
+	EmailScript.SendEmail("-t 0 -n {username} -u {verifylink} -d {email}".format(username = name, verifylink = verifylink, email = name).split(" "))
 
 	cnx.close()
 	return user_id
